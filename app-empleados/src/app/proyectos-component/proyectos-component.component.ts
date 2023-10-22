ee4ff1ff82a054a08529ba7fc248f86ee7c0c3a8
JulenR14 <julen20191414@gmail.com>
2023-10-23 00:17:09 +0200

    Boton de routing

--- app-empleados/src/app/proyectos-component/proyectos-component.component.ts
+++ app-empleados/src/app/proyectos-component/proyectos-component.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
+import { EmpleadosService } from '../empleados.service';
+import { Empleado } from '../empleado.model';
 
 @Component({
   selector: 'app-proyectos-component',
@@ -7,4 +11,26 @@ import { Component } from '@angular/core';
 })
 export class ProyectosComponentComponent {
 
+  empleados: Empleado[] = [];
+
+  nombre: string = "Nombre";
+  apellido: string = "Apellido";
+  cargo: string = "Cargo";
+  salario: number = 0;
+
+  constructor(private router:Router, private miservicio: ServicioEmpleadosService, private empleadosService: EmpleadosService){}
+  
+  ngOnInit(): void {
+    this.empleados = this.empleadosService.empleados;
+  }
+
+  volverHome(){
+    this.router.navigate([""]);
+  }
+
+  agregarEmpleado(){
+    //this.miservicio.muestraMensaje("Nombre : " + this.nombre + " Apellido : " + this.apellido);
+    this.empleadosService.agregarEmpleadoServicio(new Empleado (this.nombre, this.apellido, this.cargo, this.salario));
+    this.router.navigate([""]);
+  }
 }
